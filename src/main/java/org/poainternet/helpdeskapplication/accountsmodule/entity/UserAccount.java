ff5091d90dffd059d612ac1d047c100ed5b82cee
Bikathi Martin <bikathimartin@outlook.com>
2023-12-27 16:53:08 +0300

    Create entity to represent the user entity

--- src/main/java/org/poainternet/helpdeskapplication/accountsmodule/entity/UserAccount.java
+++ src/main/java/org/poainternet/helpdeskapplication/accountsmodule/entity/UserAccount.java
@@ -1,4 +1,47 @@
 package org.poainternet.helpdeskapplication.accountsmodule.entity;
 
-public class UserAccount {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Past;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.poainternet.helpdeskapplication.accountsmodule.definitions.UserRole;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "accounts")
+public class UserAccount implements Serializable {
+    @Serial
+    private static final long serialVersionUID = UUID.randomUUID().getLeastSignificantBits();
+
+    @Id
+    private String userId;
+
+    @NotEmpty
+    private String firstName;
+
+    @NotEmpty
+    private String lastName;
+
+    @Email
+    private String email;
+
+    @Past
+    @NotEmpty
+    private LocalDate dateOfBirth;
+    
+    private Set<UserRole> userRoles;
+
+    @NotEmpty
+    private String department;
 }
