fb0b9d71dad6db183aa66fad815af53153d437df
Bikathi Martin <bikathimartin@outlook.com>
2023-12-27 20:57:35 +0300

    Rename parent module package name

---
+++ src/main/java/org/poainternet/helpdeskapplication/securitymodule/AccountsModuleController.java
@@ -0,0 +1,9 @@
+package org.poainternet.helpdeskapplication.accountsmodule;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class AccountsModuleController {
+}
