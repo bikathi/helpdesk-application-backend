c839c513535bc38a756968572a2d59e52eb110f1
Bikathi Martin <bikathimartin@outlook.com>
2023-12-28 16:01:44 +0300

    Add custom method to get user by username

--- src/main/java/org/poainternet/helpdeskapplication/securitymodule/repository/UserAccountRepository.java
+++ src/main/java/org/poainternet/helpdeskapplication/securitymodule/repository/UserAccountRepository.java
@@ -4,6 +4,14 @@ import org.poainternet.helpdeskapplication.securitymodule.entity.UserAccount;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserAccountRepository extends MongoRepository<UserAccount, String> {
+    /**
+     *
+     * @param username is the unique username of the user tied to the account
+     * @return Optional<T> that may be empty if the user does not exist
+     */
+    Optional<UserAccount> findByUsername(String username);
 }
