2801ae4937125188ba25c35c474caa98a9315cd1
Bikathi Martin <bikathimartin@outlook.com>
2023-12-28 16:00:46 +0300

    Implement UserDetailsService logic

--- src/main/java/org/poainternet/helpdeskapplication/securitymodule/service/UserDetailsServiceImpl.java
+++ src/main/java/org/poainternet/helpdeskapplication/securitymodule/service/UserDetailsServiceImpl.java
@@ -1,5 +1,9 @@
 package org.poainternet.helpdeskapplication.securitymodule.service;
 
+import org.poainternet.helpdeskapplication.securitymodule.definitions.UserDetailsImpl;
+import org.poainternet.helpdeskapplication.securitymodule.entity.UserAccount;
+import org.poainternet.helpdeskapplication.securitymodule.repository.UserAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +11,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserAccountRepository userAccountRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        UserAccount userAccount = userAccountRepository.findByUsername(username).orElseThrow(
+            () -> new UsernameNotFoundException("Account with username " + username + " not found"));
+
+        return new UserDetailsImpl(userAccount);
     }
 }
