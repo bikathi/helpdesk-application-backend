18dfd69bc81cc07ea079c7a0daa159265d371927
Bikathi Martin <bikathimartin@outlook.com>
2023-12-28 19:52:36 +0300

    Create basic logic to handle authorization-based errors

---
+++ src/main/java/org/poainternet/helpdeskapplication/securitymodule/component/AccessDeniedJWTHandler.java
@@ -0,0 +1,18 @@
+package org.poainternet.helpdeskapplication.securitymodule.component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+@Slf4j
+public class AccessDeniedJWTHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+    }
+}
