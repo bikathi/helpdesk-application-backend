f6c3eb1fae503a21db6ac8cc7da8cda4e603b764
Bikathi Martin <bikathimartin@outlook.com>
2023-12-29 17:00:07 +0300

    Code refactorings

--- src/main/java/org/poainternet/helpdeskapplication/securitymodule/definitions/UserDetailsImpl.java
+++ src/main/java/org/poainternet/helpdeskapplication/securitymodule/definitions/UserDetailsImpl.java
@@ -7,15 +7,12 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.Locale;
 import java.util.stream.Collectors;
 
 @Getter
 public class UserDetailsImpl implements UserDetails {
     private final String userid;
     private final String username;
-    private final String fullName;
-    private final String email;
     private final String password;
     private final Boolean accountEnabled;
     private final Collection<? extends GrantedAuthority> authorities;
@@ -24,10 +21,8 @@ public class UserDetailsImpl implements UserDetails {
         this.userid = userAccount.getUserId();
         this.username = userAccount.getUsername();
         this.password = userAccount.getPassword();
-        this.email = userAccount.getEmail();
         this.accountEnabled = userAccount.getAccountEnabled();
-        this.fullName = String.format(Locale.ENGLISH, "%s %s", userAccount.getFirstName(), userAccount.getLastName());
-        this.authorities = userAccount.getUserRoles().stream().map(role -> new SimpleGrantedAuthority(role.name())).collect(Collectors.toList());
+        this.authorities = userAccount.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.name())).collect(Collectors.toList());
     }
 
     @Override
