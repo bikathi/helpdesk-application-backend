a9225df22a1739541399cfe48b2a4177700534f2
Bikathi Martin <bikathimartin@outlook.com>
2023-12-29 21:47:27 +0300

    Rename class for better legibility

---
+++ src/main/java/org/poainternet/helpdeskapplication/securitymodule/payload/response/AccDetailsResponse.java
@@ -0,0 +1,22 @@
+package org.poainternet.helpdeskapplication.securitymodule.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccDetailsResponse {
+    private String userId;
+    private String firstName;
+    private String username;
+    private String lastName;
+    private String email;
+    private String dateOfBirth;
+    private Set<String> roles;
+    private String authToken;
+    private String department;
+}
