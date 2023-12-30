0cc82b79a212b6aaa5a2ae5f8cc0035cf4af7587
Bikathi Martin <bikathimartin@outlook.com>
2023-12-30 16:45:29 +0300

    Get configuration to a working state

--- src/main/java/org/poainternet/helpdeskapplication/securitymodule/configuration/ApplicationSecurityConfiguration.java
+++ src/main/java/org/poainternet/helpdeskapplication/securitymodule/configuration/ApplicationSecurityConfiguration.java
@@ -8,18 +8,26 @@ import org.poainternet.helpdeskapplication.securitymodule.service.UserDetailsSer
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
 
 @Configuration
+@EnableWebSecurity
 @EnableMethodSecurity(
     jsr250Enabled = true,
     securedEnabled = true)
@@ -39,12 +47,22 @@ public class ApplicationSecurityConfiguration {
     }
 
     @Bean
+    public StatelessCSRFFilter statelessCSRFFilter() {
+        return new StatelessCSRFFilter();
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
     }
 
     @Bean
-    public AuthenticationProvider daoAuthenticationProvider() {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
         DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
         authenticationProvider.setUserDetailsService(userDetailsService);
         authenticationProvider.setPasswordEncoder(passwordEncoder());
@@ -53,19 +71,20 @@ public class ApplicationSecurityConfiguration {
     }
 
     @Bean
-    public DefaultSecurityFilterChain springSecurity(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain springSecurity(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-            .csrf(securityCsrfConfigurer -> securityCsrfConfigurer.disable().addFilterBefore(new StatelessCSRFFilter(), CsrfFilter.class))
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(AbstractHttpConfigurer::disable)
             .exceptionHandling(exceptionHandlingConfigurer -> {
                 exceptionHandlingConfigurer.authenticationEntryPoint(authorizationExceptionHandler);
                 exceptionHandlingConfigurer.accessDeniedHandler(accessDeniedExceptionHandler);
             })
             .sessionManagement(sessionManagementConfigurer -> sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(requestMatcherRegistry -> requestMatcherRegistry.requestMatchers("/api/v1/auth/signin").permitAll()
-            .anyRequest().authenticated());
-
-        httpSecurity.authenticationProvider(daoAuthenticationProvider());
-        httpSecurity.addFilterBefore(jwtValidationFilter(), UsernamePasswordAuthenticationFilter.class);
+            .authorizeHttpRequests(requestMatcherRegistry ->
+                requestMatcherRegistry.requestMatchers("/api/v1/auth/**").permitAll())
+            .authenticationProvider(daoAuthenticationProvider())
+            .addFilterBefore(jwtValidationFilter(), UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(statelessCSRFFilter(), CsrfFilter.class);
 
         return httpSecurity.build();
     }
