2cb54c7731123cc12e659ce31661547e4cae8c19
Bikathi Martin <bikathimartin@outlook.com>
2023-12-30 16:53:59 +0300

    Create package to house module request structures

---
+++ src/main/java/org/poainternet/helpdeskapplication/securitymodule/payload/request/SignInRequest.java
@@ -0,0 +1,13 @@
+package org.poainternet.helpdeskapplication.securitymodule.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignInRequest {
+    private String username;
+    private String password;
+}
