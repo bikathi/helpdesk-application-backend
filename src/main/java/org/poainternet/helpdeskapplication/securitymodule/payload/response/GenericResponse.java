10ca4c36f5f9523aebdc5831e240865af84afb96
Bikathi Martin <bikathimartin@outlook.com>
2023-12-30 16:57:31 +0300

    Return back annotation for builder design patterns

--- src/main/java/org/poainternet/helpdeskapplication/securitymodule/payload/response/GenericResponse.java
+++ src/main/java/org/poainternet/helpdeskapplication/securitymodule/payload/response/GenericResponse.java
@@ -1,10 +1,12 @@
 package org.poainternet.helpdeskapplication.securitymodule.payload.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class GenericResponse<T> {
