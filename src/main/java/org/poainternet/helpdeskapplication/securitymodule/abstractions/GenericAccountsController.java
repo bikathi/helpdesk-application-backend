96a511d0755081e0db098c041b772122b1db78fa
Bikathi Martin <bikathimartin@outlook.com>
2024-01-05 13:49:51 +0300

    Add new declarations to contract

--- src/main/java/org/poainternet/helpdeskapplication/securitymodule/abstractions/GenericAccountsController.java
+++ src/main/java/org/poainternet/helpdeskapplication/securitymodule/abstractions/GenericAccountsController.java
@@ -1,11 +1,12 @@
 package org.poainternet.helpdeskapplication.securitymodule.abstractions;
 
 import org.poainternet.helpdeskapplication.securitymodule.payload.request.ModifyAccRequest;
+import org.poainternet.helpdeskapplication.securitymodule.payload.request.UpdatePasswordRequest;
 import org.springframework.http.ResponseEntity;
 
 public interface GenericAccountsController {
     ResponseEntity<?> updateUserAccount(ModifyAccRequest request);
-
+    ResponseEntity<?> updateAccountPassword(UpdatePasswordRequest request);
     ResponseEntity<?> modifyUserRoles(ModifyAccRequest request);
     ResponseEntity<?> deactivateUserAccount(ModifyAccRequest request);
     ResponseEntity<?> activateUserAccount(ModifyAccRequest request);
