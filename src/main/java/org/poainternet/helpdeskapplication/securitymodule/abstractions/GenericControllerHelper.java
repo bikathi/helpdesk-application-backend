38f40aa5b13f81a45aea0b115f25d31d042f7d00
Bikathi Martin <bikathimartin@outlook.com>
2024-01-10 19:24:41 +0300

    Fix issue with roles declaration

--- src/main/java/org/poainternet/helpdeskapplication/securitymodule/abstractions/GenericControllerHelper.java
+++ src/main/java/org/poainternet/helpdeskapplication/securitymodule/abstractions/GenericControllerHelper.java
@@ -19,7 +19,7 @@ public interface GenericControllerHelper {
             switch(role.name()) {
                 case "ROLE_USER" -> userRoles.add("role_user");
                 case "ROLE_MODERATOR" -> userRoles.add("role_moderator");
-                case "ROLE_MANAGER" -> userRoles.add("role_manager");
+                case "ROLE_ADMIN" -> userRoles.add("role_admin");
             }
         });
 
@@ -32,7 +32,7 @@ public interface GenericControllerHelper {
             switch(role) {
                 case "role_user" -> roles.add(UserRole.ROLE_USER);
                 case "role_moderator" -> roles.add(UserRole.ROLE_MODERATOR);
-                case "role_manager" -> roles.add(UserRole.ROLE_MANAGER);
+                case "role_admin" -> roles.add(UserRole.ROLE_ADMIN);
                 default -> throw new InternalServerError(String.format("Undefined role %s", role));
             }
         }
