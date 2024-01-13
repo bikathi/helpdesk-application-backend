17ac1b7f8ee24cf2c3d60f8bdcac49916e465658
Bikathi Martin <bikathimartin@outlook.com>
2024-01-13 23:27:24 +0300

    Complete service method to work with searching for user account records

--- src/main/java/org/poainternet/helpdeskapplication/securitymodule/service/UserAccountService.java
+++ src/main/java/org/poainternet/helpdeskapplication/securitymodule/service/UserAccountService.java
@@ -3,18 +3,20 @@ package org.poainternet.helpdeskapplication.securitymodule.service;
 import lombok.extern.slf4j.Slf4j;
 import org.poainternet.helpdeskapplication.securitymodule.abstractions.GenericAccountsService;
 import org.poainternet.helpdeskapplication.securitymodule.entity.UserAccount;
+import org.poainternet.helpdeskapplication.securitymodule.definitions.SearchCriteriaDefinition;
+import org.poainternet.helpdeskapplication.securitymodule.util.ModuleUtil;
 import org.poainternet.helpdeskapplication.sharedexceptions.EntityNotFoundException;
 import org.poainternet.helpdeskapplication.sharedexceptions.InternalServerError;
 import org.poainternet.helpdeskapplication.securitymodule.repository.UserAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Locale;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -22,6 +24,9 @@ import java.util.stream.Collectors;
 public class UserAccountService implements GenericAccountsService<UserAccount> {
     @Autowired
     private UserAccountRepository userAccountRepository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
     @Override
     public UserAccount getAccountByUsername(String username) throws EntityNotFoundException {
@@ -53,5 +58,11 @@ public class UserAccountService implements GenericAccountsService<UserAccount> {
     @Override
     public void saveAccountDetails(UserAccount account) {
         userAccountRepository.save(account);
+    }
+
+    @Override
+    public List<UserAccount> searchAccountsByCriteria(SearchCriteriaDefinition searchCriteria) {
+        Query searchQuery =  ModuleUtil.generateSearchQuery(searchCriteria);
+        return mongoTemplate.find(searchQuery, UserAccount.class);
     }
 }
