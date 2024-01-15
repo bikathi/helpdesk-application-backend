0e90bf5546cb80e401ced78f5245c4c92108f376
Bikathi Martin <bikathimartin@outlook.com>
2024-01-15 20:18:33 +0300

    Improve on declaration of the userRole ENUM

--- src/main/java/org/poainternet/helpdeskapplication/securitymodule/definitions/UserRole.java
+++ src/main/java/org/poainternet/helpdeskapplication/securitymodule/definitions/UserRole.java
@@ -1,7 +1,16 @@
 package org.poainternet.helpdeskapplication.securitymodule.definitions;
 
+import lombok.Getter;
+
+@Getter
 public enum UserRole {
-    ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN
+    ROLE_USER("role_user"),
+    ROLE_MODERATOR("role_moderator"),
+    ROLE_ADMIN("role_admin");
+
+    private final String label;
+
+    UserRole(String label) {
+        this.label = label;
+    }
 }
