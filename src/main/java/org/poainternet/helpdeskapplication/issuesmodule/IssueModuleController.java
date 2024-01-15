f837ed2e2125c829d2d8615940e02c8500ab00c8
Bikathi Martin <bikathimartin@outlook.com>
2024-01-15 14:20:36 +0300

    Create basic structure for issues controller

--- src/main/java/org/poainternet/helpdeskapplication/issuesmodule/IssueModuleController.java
+++ src/main/java/org/poainternet/helpdeskapplication/issuesmodule/IssueModuleController.java
@@ -1,17 +1,44 @@
 package org.poainternet.helpdeskapplication.issuesmodule;
 
 import lombok.extern.slf4j.Slf4j;
+import org.poainternet.helpdeskapplication.issuesmodule.abstractions.GenericClientIssueController;
 import org.poainternet.helpdeskapplication.issuesmodule.service.ClientIssueService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequestMapping(value = "/api/v1/issues")
-public class IssueModuleController {
+public class IssueModuleController implements GenericClientIssueController {
     private final String CLASS_NAME = this.getClass().getName();
 
     @Autowired
     private ClientIssueService clientIssueService;
+
+    @Override
+    public ResponseEntity<?> openNewIssue() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> markIssueAsClosed() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> searchIssues() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> getListOfIssues() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> getIssueById() {
+        return null;
+    }
 }
