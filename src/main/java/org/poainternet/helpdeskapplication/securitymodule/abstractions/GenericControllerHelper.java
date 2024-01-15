bcf4f7f655941a27b9a5397c20877f0ee0c1c93b
Bikathi Martin <bikathimartin@outlook.com>
2024-01-15 20:17:59 +0300

    Improve functionality for mapping String[] roles to ENUM[] and back

--- src/main/java/org/poainternet/helpdeskapplication/securitymodule/abstractions/GenericControllerHelper.java
+++ src/main/java/org/poainternet/helpdeskapplication/securitymodule/abstractions/GenericControllerHelper.java
@@ -15,27 +15,13 @@ public interface GenericControllerHelper {
 
     default HashSet<String> roleEnumColToStringCol(Set<UserRole> roles) {
         HashSet<String> userRoles = new HashSet<>();
-        roles.forEach(role -> {
-            switch(role.name()) {
-                case "ROLE_USER" -> userRoles.add("role_user");
-                case "ROLE_MODERATOR" -> userRoles.add("role_moderator");
-                case "ROLE_ADMIN" -> userRoles.add("role_admin");
-            }
-        });
-
+        roles.forEach(role ->  userRoles.add(role.getLabel()));
         return userRoles;
     }
 
     default Set<UserRole> stringColToRoleEnumCol(String[] userRoles) throws InternalServerError {
         Set<UserRole> roles = new HashSet<>();
-        for(String role : userRoles) {
-            switch(role) {
-                case "role_user" -> roles.add(UserRole.ROLE_USER);
-                case "role_moderator" -> roles.add(UserRole.ROLE_MODERATOR);
-                case "role_admin" -> roles.add(UserRole.ROLE_ADMIN);
-                default -> throw new InternalServerError(String.format("Undefined role %s", role));
-            }
-        }
+        for(String role : userRoles) { roles.add(UserRole.valueOf(role)); }
         return roles;
     }
 
