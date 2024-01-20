e28481d60814356b01718b67f35e2605742d7b28
Bikathi Martin <bikathimartin@outlook.com>
2024-01-20 22:20:48 +0300

    Add basic implementation of abstraction def

--- src/main/java/org/poainternet/helpdeskapplication/commentsmodule/DiscussionsController.java
+++ src/main/java/org/poainternet/helpdeskapplication/commentsmodule/DiscussionsController.java
@@ -2,17 +2,20 @@ package org.poainternet.helpdeskapplication.commentsmodule;
 
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.poainternet.helpdeskapplication.commentsmodule.abstractions.GenericCommentsController;
+import org.poainternet.helpdeskapplication.commentsmodule.payload.response.AddCommentRequest;
+import org.poainternet.helpdeskapplication.commentsmodule.payload.response.UpdateCommentRequest;
 import org.poainternet.helpdeskapplication.commentsmodule.service.CommentService;
 import org.poainternet.helpdeskapplication.securitymodule.SecurityModuleShareable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping(value = "/api/v1/comments")
-public class DiscussionsController {
+public class DiscussionsController implements GenericCommentsController {
     private final String CLASS_NAME = this.getClass().getName();
 
     @Value("${application.api.version}")
@@ -29,4 +32,19 @@ public class DiscussionsController {
 
     @Autowired
     private CommentService commentService;
+
+    @Override
+    public ResponseEntity<?> addNewComment(@RequestBody AddCommentRequest request) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> updateComment(@RequestBody UpdateCommentRequest request) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> getCommentList(@RequestParam String issueId, @RequestParam Integer page) {
+        return null;
+    }
 }
