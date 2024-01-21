aba2a3427190f5a1ac1c4a8b79c178c302ea1261
Bikathi Martin <bikathimartin@outlook.com>
2024-01-21 19:51:02 +0300

    Add service structure for newer module endpoints

--- src/main/java/org/poainternet/helpdeskapplication/commentsmodule/abstractions/GenericCommentService.java
+++ src/main/java/org/poainternet/helpdeskapplication/commentsmodule/abstractions/GenericCommentService.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface GenericCommentService {
     List<Comment> getCommentList(String issueId, Integer page);
+    void saveNewComment(Comment comment);
+    Comment getCommentById(String commentId);
 }
--- src/main/java/org/poainternet/helpdeskapplication/commentsmodule/service/CommentService.java
+++ src/main/java/org/poainternet/helpdeskapplication/commentsmodule/service/CommentService.java
@@ -3,6 +3,7 @@ package org.poainternet.helpdeskapplication.commentsmodule.service;
 import org.poainternet.helpdeskapplication.commentsmodule.abstractions.GenericCommentService;
 import org.poainternet.helpdeskapplication.commentsmodule.entity.Comment;
 import org.poainternet.helpdeskapplication.commentsmodule.repository.CommentRepository;
+import org.poainternet.helpdeskapplication.sharedexceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -19,5 +20,16 @@ public class CommentService implements GenericCommentService {
     public List<Comment> getCommentList(String issueId, Integer page) {
         Pageable pageInfo = PageRequest.of(page, 10);
         return commentRepository.findAllByIssueId(issueId, pageInfo).toList();
+    }
+
+    @Override
+    public void saveNewComment(Comment comment) {
+        commentRepository.save(comment);
+    }
+
+    @Override
+    public Comment getCommentById(String commentId) {
+        return commentRepository.findById(commentId).orElseThrow(
+            () -> new EntityNotFoundException(String.format("Comment with id %s not found", commentId)));
     }
 }
